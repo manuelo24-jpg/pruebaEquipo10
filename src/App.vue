75dd795877c0802060eaf1004b3ec9918fb242c8
Manuelo69 <nomail@gmail.com>
2024-08-21 10:37:36 +0200

    Todo organizado, muestra los datos y guarda, queda poner todo en español

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,62 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-import DatosHeroeFormulario from "./components/DatosHeroeFormulario.vue";
+import { ref } from "vue";
 import FormularioHeroe from "./components/FomularioHeroe.vue";
+import DatosHeroeFormulario from "./components/DatosHeroeFormulario.vue";
 import DatosHeroes from "./components/DatosHeroes.vue";
 
-const heroes = [
+const heroes = ref([
   { nombre: "Batman", liga: "liga2", peso: 85, raza: "humano" },
   { nombre: "Superman", liga: "liga2", peso: 95, raza: "kryptoniano" },
   { nombre: "Mujer Maravilla", liga: "liga3", peso: 60, raza: "humana" },
   { nombre: "Flash", liga: "liga1", peso: 67, raza: "humano" },
   { nombre: "Linterna", liga: "liga2", peso: 79, raza: "Mandaloriano" },
   { nombre: "Americaman", liga: "liga11", peso: 95, raza: "Estadounidense" },
-];
+]);
+
+const nuevoHeroe = ref({
+  nombre: "",
+  liga: "",
+  peso: 0,
+  raza: "",
+});
+
+const agregarHeroe = () => {
+  heroes.value.push({ ...nuevoHeroe.value });
+  nuevoHeroe.value = { nombre: "", liga: "", peso: 0, raza: "" };
+};
 </script>
 
 <template>
-  <div>
-    <h1>Insercción Héroe</h1>
-   <FormularioHeroe/>
+  <div class="contenedor">
+    <div class="formularioInsercion">
+      <h1>Insercción Héroe</h1>
+      <FormularioHeroe
+        v-model:nuevoHeroe="nuevoHeroe"
+        @agregarHeroe="agregarHeroe"
+      />
+    </div>
+    <div class="heroeInsertado">
+      <h1>Héroe insertado</h1>
+      <DatosHeroeFormulario v-bind="nuevoHeroe" />
+    </div>
+    <div>
+      <DatosHeroes :heroes="heroes" />
+    </div>
   </div>
-  <div>
-    <h1>Héroe insertado</h1>
-   
-      <DatosHeroeFormulario/>
- 
-  </div>
-  <DatosHeroes :heroes="heroes" />
 </template>
 
 <style scoped>
+.contenedor {
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+}
+
+.formularioInsercion,
+.heroeInsertado {
+  margin-top: 1rem;
+}
+
 header {
   line-height: 1.5;
 }
--- src/components/FomularioHeroe.vue
+++ src/components/FomularioHeroe.vue
@@ -1,22 +1,41 @@
 <script setup>
-import { ref } from "vue";
-const nombre= ref("Aquí va el nombre del héroe");
-const liga= ref("liga2");
-const peso= ref(85);
-const raza= ref("humano");
+import { ref, watch } from "vue";
+import { defineEmits, defineProps } from "vue";
+
+const props = defineProps({
+  nuevoHeroe: {
+    type: Object,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["update:nuevoHeroe", "agregarHeroe"]);
+
+const nombre = ref(props.nuevoHeroe.nombre);
+const liga = ref(props.nuevoHeroe.liga);
+const peso = ref(props.nuevoHeroe.peso);
+const raza = ref(props.nuevoHeroe.raza);
+
+watch([nombre, liga, peso, raza], ([newNombre, newLiga, newPeso, newRaza]) => {
+  emit("update:nuevoHeroe", {
+    nombre: newNombre,
+    liga: newLiga,
+    peso: newPeso,
+    raza: newRaza,
+  });
+});
 </script>
 
 <template>
-    <form>
-        <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" v-model="nombre" />
-        <label for="liga">Liga:</label>
-        <input type="text" id="liga" v-model="liga" />
-        <label for="peso">Peso:</label>
-        <input type="number" id="peso" v-model="peso" />
-        <label for="raza">Raza:</label>
-        <input type="text" id="raza" v-model="raza" />
-        <button type="submit">Enviar</button>
-    </form>
-
+  <form @submit.prevent="emit('agregarHeroe')">
+    <label for="nombre">Nombre:</label>
+    <input type="text" id="nombre" v-model="nombre" />
+    <label for="liga">Liga:</label>
+    <input type="text" id="liga" v-model="liga" />
+    <label for="peso">Peso:</label>
+    <input type="number" id="peso" v-model="peso" />
+    <label for="raza">Raza:</label>
+    <input type="text" id="raza" v-model="raza" />
+    <button type="submit">Enviar</button>
+  </form>
 </template>
